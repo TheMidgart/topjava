eed22c6e20559949983833f14e0c8af331f6f09b
Midgart <TheMidgart@gmail.com>
2022-11-08 01:01:13 +0300

    HW5 add hsql/postgres repos for jdbc

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryPostgres.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.Profiles;
+
+import java.time.LocalDateTime;
+
+@Repository
+@Profile(value = {Profiles.POSTGRES_DB})
+public class JdbcMealRepositoryPostgres extends JdbcMealRepository <LocalDateTime> {
+    public JdbcMealRepositoryPostgres(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    public LocalDateTime getCorrectDateTime(LocalDateTime dateTime) {
+        return  dateTime;
+    }
+}
