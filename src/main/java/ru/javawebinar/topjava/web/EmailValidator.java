698a0fde2745aafab32a93789ec13d408d0e0f55
Midgart <themidgart@gmail.com>
2022-12-21 12:52:48 +0300

    HW10-3 fix EmailValidator

--- src/main/java/ru/javawebinar/topjava/web/EmailValidator.java
+++ src/main/java/ru/javawebinar/topjava/web/EmailValidator.java
@@ -1,19 +1,17 @@
 package ru.javawebinar.topjava.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.to.UserTo;
+import ru.javawebinar.topjava.util.UsersUtil;
 
 
 @Component
 public class EmailValidator implements Validator {
-    @Autowired
-    ApplicationContext context;
     @Autowired
     UserRepository repository;
 
@@ -23,19 +21,27 @@ public class EmailValidator implements Validator {
 
     @Override
     public boolean supports(Class<?> clazz) {
-        return clazz.equals(UserTo.class);
+        return clazz.equals(UserTo.class) || clazz.equals(User.class);
     }
 
     @Override
     public void validate(Object obj, Errors errors) {
-        UserTo user = (UserTo) obj;
+        UserTo user;
+        if (obj instanceof User) {
+            user = UsersUtil.asTo((User) obj);
+        } else {
+            user = (UserTo) obj;
+        }
         String email = user.getEmail().toLowerCase();
         User currUser = repository.getByEmail(email);
         if (currUser != null) {
             if (!currUser.getId().equals(user.getId())) {
-//                if (!currUser.getId().equals(SecurityUtil.authUserId()))
-                    errors.rejectValue("email", "email.already.exists", "This email already exists");
+                if (!currUser.getId().equals(SecurityUtil.authUserId()))
+                    errors.rejectValue("email", "email.already.exists",
+                            "This email already exists");
             }
         }
     }
+
+
 }
